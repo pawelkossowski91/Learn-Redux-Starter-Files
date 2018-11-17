f7d2acbf88234570d2ede0152419cac9480ba469
Paweł Kossowski <pawelkossowski91@gmail.com>
2018-11-17 22:25:36 +0100

    component up

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+// import { Single } from './Single';
+
+const Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>
+                <Link to="/"><h1>Reduxstagram</h1></Link>
+                </h1>
+                {React.cloneElement(this.props.children, this.props)}
+            </div>
+        )
+    }
+});
+
+export default Main;
---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+           <div className="photo-grid">
+              <p>I'm the photgrid</p> 
+           </div>
+        )
+    }
+});
+
+export default PhotoGrid;
